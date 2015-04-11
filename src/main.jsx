0d98f9e074037f79e94563aa0750714ee93ac4c4
yapcheahshen <yapcheahshen@gmail.com>
2015-04-11 23:25:55 +0800

    infinite scroll kdb text

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
-var React=require("react");
+var React=require("react/addons");
+
 var kse=require("ksana-search");
-var FlattenView = require('ksana-layer-react').FlattenView;
+var kde=require("ksana-database");
+
+var BaseView = require("ksana-layer-react").BaseView;
+var Infinite=require("ksana2015-components").infinite;
 var styles={
   fontSize:"24px"
 }
@@ -11,8 +15,8 @@ var markupStyles={
   ,hl0:{backgroundColor:"red",color:"yellow"}
   ,hl1:{backgroundColor:"green",color:"yellow"}
 };
+
 var convertSearchResult=function(sr) {
-  console.log(sr)
   return sr.excerpt.map(function(e){
     var hits=e.realHits.map(function(hit){
       return [hit[0],hit[1],{type:"hl"+hit[2]}];
@@ -20,27 +24,53 @@ var convertSearchResult=function(sr) {
     return {text:e.text,hits:hits};
   });
 }
+
+var Item=React.createClass({
+  mixins: [React.addons.PureRenderMixin]
+  ,onselect:function(start,len,thechar) {
+    console.log(this.props.index,start,len,thechar);
+  }
+  //markups={item.hits}  markupStyles={markupStyles}
+  ,render:function() {
+      return <BaseView onSelect={this.onselect} text={(this.props.text||"")}/>
+  }
+});
+
 var MainComponent = React.createClass({
   componentDidMount:function() {
+    /*
     var that=this;
     kse.search("cbeta","優婆塞 比丘",{realPos:true,nohighlight:true,range:{start:0,maxfile:5}},function(err,result){
       if (err)return;
       that.setState({rows:convertSearchResult(result)});
     });
+*/
+    kde.open("cbeta",function(err,db){
+      this.setState({db:db,moredata:this.moredata});
+    },this);
+
+  }
+  ,moredata:function(now,cb) {
+    var keys=[];;
+    var t="";
+    for (var i=0;i<100;i++) {
+      var fseg=this.state.db.absSegToFileSeg(now+i);
+      keys.push(["filecontents",fseg.file,fseg.seg]);
+    }
+    this.state.db.get(keys,function(data){
+      cb(data);
+    })
   }
   ,getInitialState:function() {
     return {rows:[],markups:[[1,3,{type:"warn"}],[2,3,{type:"important"}] ]}
   }
-  ,onselect:function(start,len,thechar) {
-    console.log(start,len,thechar);
-  }
+
   ,renderItem:function(item,idx) {
-      return <FlattenView key={idx} onSelect={this.onselect} text={item.text}
-         markups={item.hits} markupStyles={markupStyles}/>
+
   }
   ,render: function() {
-     return <div style={styles}>
-        {this.state.rows.map(this.renderItem)}
+     return <div className="infinite-example" style={styles}>
+        <Infinite onLoad={this.state.moredata} itemClass={Item} height={500}/>
      	</div>
   }
 
